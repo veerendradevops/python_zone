e4705642f842987d69d0f4046318e83acb79c533
veerendradevops <veerendratholeti@gmail.com>
2019-08-26 10:43:06 +0000

    added

---
+++ nest_list_tuple.py
@@ -0,0 +1,11 @@
+Employees = [(101,"Veeren",25),(102,"Mani",25),(103,"Ram",25)]
+print("---------printing list--------------")
+for i in Employees:
+    print(i)
+Employees[0]= [444,"Veerendra",22]
+print(Employees);
+print("---------printing after modification---------------")
+for i in Employees:
+    print(i)
+
+
---
+++ set.py
@@ -0,0 +1,6 @@
+Days = {"Mon","Tues","Wednes","Thurs","Fri","Satur","Sun"}
+print(Days)
+print(type(Days))
+print("loopng through the set elements")
+for i in Days:
+    print(i)
